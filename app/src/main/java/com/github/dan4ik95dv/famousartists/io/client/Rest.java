63f8146942bac044fd5dd12ea5d7ca55dbfb8f80
Daniil Celikin <celikin@mayak.red>
2016-04-09 22:40:51 +1000

    added Rest interface

---
+++ app/src/main/java/com/github/dan4ik95dv/famousartists/io/client/Rest.java
@@ -0,0 +1,22 @@
+package com.github.dan4ik95dv.famousartists.io.client;
+
+import com.github.dan4ik95dv.famousartists.model.Artist;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import rx.Observable;
+
+/**
+ * Created by Daniil Celikin on 09.04.2016.
+ * https://github.com/dan4ik95dv/famousartists
+ */
+public interface Rest {
+
+    //Get Famous Artists
+    @Headers({"Accept: application/json", "Content-Type: application / json"})
+    @GET("/mobilization-2016/artists.json")
+    Observable<List<Artist>> getArtists();
+
+}
