e251db090e4fdff011801cd1b308f493336c4003
Daniil Celikin <celikin@mayak.red>
2016-04-09 22:32:36 +1000

    added models Artist and Cover

---
+++ app/src/main/java/com/github/dan4ik95dv/famousartists/model/Artist.java
@@ -0,0 +1,165 @@
+package com.github.dan4ik95dv.famousartists.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Daniil Celikin on 09.04.2016.
+ * https://github.com/dan4ik95dv/famousartists
+ */
+
+@Generated("org.jsonschema2pojo")
+public class Artist {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("genres")
+    @Expose
+    private List<String> genres = new ArrayList<String>();
+    @SerializedName("tracks")
+    @Expose
+    private Integer tracks;
+    @SerializedName("albums")
+    @Expose
+    private Integer albums;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("cover")
+    @Expose
+    private Cover cover;
+
+    /**
+     *
+     * @return
+     * The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     * The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     * The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     * The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     * The genres
+     */
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    /**
+     *
+     * @param genres
+     * The genres
+     */
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    /**
+     *
+     * @return
+     * The tracks
+     */
+    public Integer getTracks() {
+        return tracks;
+    }
+
+    /**
+     *
+     * @param tracks
+     * The tracks
+     */
+    public void setTracks(Integer tracks) {
+        this.tracks = tracks;
+    }
+
+    /**
+     *
+     * @return
+     * The albums
+     */
+    public Integer getAlbums() {
+        return albums;
+    }
+
+    /**
+     *
+     * @param albums
+     * The albums
+     */
+    public void setAlbums(Integer albums) {
+        this.albums = albums;
+    }
+
+    /**
+     *
+     * @return
+     * The description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     *
+     * @param description
+     * The description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     *
+     * @return
+     * The cover
+     */
+    public Cover getCover() {
+        return cover;
+    }
+
+    /**
+     *
+     * @param cover
+     * The cover
+     */
+    public void setCover(Cover cover) {
+        this.cover = cover;
+    }
+
+}
